7340df6cd29648f526387fd502f12c212531b4fd
Lehan Ouyang <lo739@nyu.edu>
2022-11-12 10:27:48 -0500

    adding project cards

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -84,7 +84,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/lehan0328/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
